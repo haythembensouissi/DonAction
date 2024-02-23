1c85416257f826cab49cc31ed610fcf5d229e8b9
abdou50 <gadourraddadi@gmail.com>
2024-02-23 18:48:15 +0100

    l

---
+++ client/src/app/news.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NewsItem from './components/newsitems';
+
+const News = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+          params: {
+            country: 'us',
+            apiKey: '1061edad44a642e9af7dde41b317ef6c',
+          },
+        });
+        setNews(response.data.articles);
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
+  console.log(news);
+
+  return (
+    <div>
+      <h2 className='text-center'>Latest <span className="badge text-bg-danger">News</span></h2>
+      {news.map((newsItem, index) => (
+        <NewsItem
+          key={index}
+          title={newsItem.title}
+          description={newsItem.description}
+          src={newsItem.urlToImage}
+          url={newsItem.url}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default News;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,9 +5,11 @@ import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 import ToggleForms from './components/toggleForms';
 import  './components/styles.css';
-
+import Navbar from './components/Navbar';
+import News from './news';
+import newsitems from './components/newsitems';
 const Page = () => {
-  const handleSignUp = () => {
+  /**const handleSignUp = () => {
     console.log('Handling sign up...');
   };
  
@@ -15,10 +17,14 @@ const Page = () => {
     console.log('Handling sign in...');
   };
 
-  return (
-    <div className="container">
+  **/return (
+    /**<div className="container">
       
       <ToggleForms handleSignUp={handleSignUp} handleSignIn={handleSignIn} />
+    </div>**/
+    <div>
+    <Navbar />
+    <News/>
     </div>
   );
 };
