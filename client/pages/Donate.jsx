d753019464f74fce7aaedc75aa93796e6f1babab
abdou50 <gadourraddadi@gmail.com>
2024-04-02 14:50:03 +0100

    donation form and profile section

--- client/pages/Donate.jsx
+++ client/pages/Donate.jsx
@@ -1,10 +1,14 @@
-"use client"
-import React from 'react'
-
-function Donate() {
+import React from 'react';
+import DonationForm from '../src/app/components/DonationForm';;
+import Navbar from '../src/app/components/Navbar'
+const Donate = () => {
   return (
-    <div>Donate</div>
-  )
-}
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <Navbar />
+        <h1 className="text-3xl text-center text-red-600  py-2 px-4">Make a Donation</h1>
+        <DonationForm />
+      </div>
+    );
+};
 
-export default Donate
+export default Donate;
